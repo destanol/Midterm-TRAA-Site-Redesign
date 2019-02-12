4b1b2c5aada714f022347ea0bbc757a758597e95
destanol <dav.estanol@gmail.com>
2019-02-12 00:55:19 -0500

    CSS Polish
    -font Lato
    +Added Work Sans
    +Layout tweaks

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-//create a pickel jar, label it as firstName, put a string in it.
-//var firstName = "Mallory";
 
-//alert("Howdy" + " " + firstName);
-
-//mordern js
-
-//alert(`Hello ${firstName} !!!`);
-
-console.log("Javascript is Linked up");
+// function modalPopup() {
+//     alert('ok');
+// }
+// window.onload = modalPopup;
